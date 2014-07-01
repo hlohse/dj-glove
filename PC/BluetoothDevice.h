775ebdbcbf96a04a65c1e46bb0c1e60db276c068
Henning Lohse <henning.lohse@gmail.com>
2014-07-01 12:55:01 +0200

    Ported BluetoothDevice to Windows

--- PC/BluetoothDevice.h
+++ PC/BluetoothDevice.h
@@ -20,29 +20,49 @@
 
 #include <string>
 #include <cstdint>
+
+#ifdef __linux__
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
+#elif _WIN32
+//#include <Windows.h>
+#include <WinSock2.h>
+#include <ws2bth.h>
+#include <bthsdpdef.h>
+#include <bluetoothapis.h>
+#endif
 
 class BluetoothDevice {
 public:
     static const BluetoothDevice Arduino;
 
+#ifdef __linux__
+	using SocketAddress = struct sockaddr_rc;
+#elif _WIN32
+	using SocketAddress = SOCKADDR_BTH;
+#endif
+
     BluetoothDevice(const std::string& name,
                     const std::string& address,
                     const int channel);
     ~BluetoothDevice();
-    
-    struct sockaddr_rc GetSocketAddress() const;
 
     bool IsValid() const;
-    std::string ToString() const;
+	std::string ToString() const;
+
+	SocketAddress GetSocketAddress() const;
 
 private:
     std::string name_;
     std::string address_;
     int channel_;
-    struct sockaddr_rc socket_address_;
-    bool is_valid_;
+	bool is_valid_;
+	SocketAddress socket_address_;
+
+#ifdef _WIN32
+	int StringToAddress(IN const std::string& address_string,
+						OUT BTH_ADDR& address);
+#endif
 };
 
 #endif // DJ_GLOVE_PC_BLUETOOTH_DEVICE_H_
