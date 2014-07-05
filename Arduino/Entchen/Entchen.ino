8aa66cd19dbc5b512b90f0cf38a683d5b8c363ac
Henning Lohse <henning.lohse@gmail.com>
2014-07-05 11:49:36 +0200

    Implemented Entchen Arduino project. After receiving start signal '!', it sends MIDI signals playing Alle meine Entchen via Bluetooth :)

---
+++ Arduino/Entchen/Entchen.ino
@@ -0,0 +1,39 @@
+#include <Midi.h>
+#include <MidiChannel.h>
+#include <Bluetooth.h>
+#include <SoftwareSerial.h>
+
+const char start_signal = '!';
+const int  channel_nr   = 0;
+const int  velocity     = 64;
+const int  key_delay    = 100;
+const int  num_keys     = 27;
+
+// http://computermusicresource.com/midikeys.html
+const int keys[num_keys] = {
+    60, 62, 64, 65, 67, 67,  // C D E F G G
+    69, 69, 69, 69, 67,      // A A A A G
+    69, 69, 69, 69, 67,      // A A A A G
+    65, 65, 65, 65, 64, 64,  // F F F F E E
+    62, 62, 62, 62, 60       // D D D D C
+};
+
+void setup() {
+    BluetoothSetup();
+}
+
+void loop() {
+    MidiChannel channel(channel_nr);
+    channel.Status(Midi::NoteOn);
+    channel.Velocity(velocity);
+    
+    while (!Bluetooth.available());
+    
+    if (Bluetooth.read() == start_signal) {
+        for (int i = 0; i < num_keys; i++) {
+            channel.Key(keys[i]);
+            Bluetooth.print(channel.Signal());
+            delay(key_delay);
+        }
+    }
+}
