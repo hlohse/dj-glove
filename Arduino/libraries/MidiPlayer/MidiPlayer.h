4f34edba86f1d5ee1d3a7eb84e468fb4e28c5821
Henning Lohse <henning.lohse@gmail.com>
2014-07-05 14:05:48 +0200

    MidiPlayer comments

--- Arduino/libraries/MidiPlayer/MidiPlayer.h
+++ Arduino/libraries/MidiPlayer/MidiPlayer.h
@@ -1,3 +1,19 @@
+/*      MidiPlayer
+ *
+ *  Provides a set of channels to be configured with MIDI signals that can be
+ *  send via Bluetooth to be played.
+ *
+ *  Example, play middle C (60) on channel 5;
+ *
+ *      MidiPlayer player;
+ *
+ *      player(5).SetStatus(Midi::NoteOn);
+ *      player(5).SetKey(60);
+ *      player(5).SetVelocity(64);
+ *
+ *      player.Play(5);
+ */
+
 #ifndef DJ_GLOVE_ARDUINO_LIBRARY_MIDI_PLAYER_H_
 #define DJ_GLOVE_ARDUINO_LIBRARY_MIDI_PLAYER_H_
 
