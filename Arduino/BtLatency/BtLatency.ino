62de7ca71d9ada546bf91ebcb55869207265757d
Henning Lohse <henning.lohse@gmail.com>
2014-07-10 09:54:13 +0200

    Arduino BtLatency can switch between software and hardware serial via define falg

--- Arduino/BtLatency/BtLatency.ino
+++ Arduino/BtLatency/BtLatency.ino
@@ -1,6 +1,14 @@
 #include <Bluetooth.h>
 #include <SoftwareSerial.h>
 
+#define USE_SOFTWARE_SERIAL 1 // Comment out to use hardware Serial pins 0, 1
+
+#ifdef USE_SOFTWARE_SERIAL
+#define _SERIAL_ Bluetooth
+#else
+#define _SERIAL_ Serial
+#endif
+
 // NOTE: USE THIS VALUES IN PC PROJECT, TOO!
 const unsigned int num_messages = 100;
 const char*        message      = "PING";
@@ -8,15 +16,19 @@ const char         start_signal = '!';
 
 void setup()  
 {
+#ifdef USE_SOFTWARE_SERIAL
     BluetoothSetup();
+#else
+    Serial.begin(115200);
+#endif
 }
 
 void loop()
 {
-    if (Bluetooth.available()) {
-        if (Bluetooth.read() == start_signal) {
+    if (_SERIAL_.available()) {
+        if (_SERIAL_.read() == start_signal) {
             for (unsigned int i = 0; i < num_messages; ++i) {
-                Bluetooth.print(message);
+                _SERIAL_.print(message);
             }
         }
     }
