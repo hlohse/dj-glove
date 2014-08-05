7702b2988de7f8ecabf4fb4c51ced39cfcbf65a0
Henning Lohse <henning.lohse@gmail.com>
2014-08-05 14:24:31 +0200

    * Copied Midi to PC * Ported to C++11

---
+++ PC/Midi.cpp
@@ -0,0 +1,49 @@
+#include "Midi.h"
+#include <algorithm>
+using namespace std;
+
+Midi::byte_t Midi::Limit(const Midi::byte_t value,
+                         const Midi::ValueType type)
+{
+    const byte_t min = static_cast<byte_t>(Limits::Min);
+    const byte_t max = type == ValueType::Channel ?
+        static_cast<byte_t>(Limits::ChannelMax) : 
+        static_cast<byte_t>(Limits::Max);
+
+    return std::min(std::max(value, min), max);
+}
+
+void Midi::SplitStatusChannel(const Midi::byte_t status_channel,
+                              Midi::Status& status,
+                              Midi::byte_t& channel)
+{
+    status  = static_cast<Midi::Status>(status_channel & 0xF0); // 1111 0000
+    channel = status_channel & 0x0F; // 0000 1111
+}
+
+Midi::byte_t Midi::CombineStatusChannel(const Midi::Status status,
+                                        const Midi::byte_t channel)
+{
+    return static_cast<byte_t>(status) | (channel & 0x0F); // ssss cccc
+}
+
+void Midi::SplitPitchBend(const Midi::word_t pitch_bend,
+                          Midi::byte_t& fine,
+                          Midi::byte_t& coarse)
+{
+    const byte_t fine_7bit   = (byte_t) (pitch_bend & 0x007F); // 0000 0000 0111 1111
+    const byte_t coarse_7bit = (byte_t) ((pitch_bend >> 7) & 0x007F);
+
+    fine   = Limit(fine_7bit,   ValueType::Value);
+    coarse = Limit(coarse_7bit, ValueType::Value);
+}
+
+Midi::word_t Midi::CombinePitchBend(const Midi::byte_t fine,
+                                    const Midi::byte_t coarse)
+{
+    const word_t fine_word   = ((word_t) fine) & 0x007F;          // 0000 0000 0111 1111
+    const word_t coarse_word = (((word_t) coarse) << 7) & 0x3F80; // 0011 1111 1000 0000
+    
+    return coarse_word | fine_word; // 00cc cccc cfff ffff
+}
+
---
+++ PC/Midi.h
@@ -0,0 +1,137 @@
+/*      Midi
+ *
+ *  http://www.indiana.edu/~emusic/etext/MIDI/chapter3_MIDI4.shtml
+ *
+ *  Status   | Data 1   | Data 2   | Message           
+ * ----------------------------------------------------
+ *  1000nnnn | 0kkkkkkk | 0vvvvvvv | Note Off          
+ *  1001nnnn | 0kkkkkkk | 0vvvvvvv | Note On           
+ *  1010nnnn | 0kkkkkkk | 0ppppppp | Poly Key Pressure 
+ *  1011nnnn | 0ccccccc | 0cvcvcvc | Controller Change 
+ *  1100nnnn | 0preprep |  <none>  | Program Change    
+ *  1101nnnn | 0ppppppp |  <none>  | Channel Pressure  
+ *  1110nnnn | 0fffffff | 0cocococ | Pitch Bend         
+ *
+ *  Legend:
+ *      n   Channel             0-15
+ *      k   Key #               0-127 (60 = middle C)
+ *      v   Velocity            0-127
+ *      p   Pressure            0-127
+ *      c   Controller          See MidiChannel::Controller below
+ *      cv  Controller value    0-127
+ *      pre Preset              0-127
+ *      f   Fine                co+f = 14 bit resolution
+ *      co  Coarse              co+f = 14 bit resolution
+ */
+
+#ifndef DJ_GLOVE_PC_MIDI_H_
+#define DJ_GLOVE_PC_MIDI_H_
+
+#include <cstdint>
+
+class Midi {
+public:
+    using byte_t = uint8_t;  // Unsigned for 0..127 with 7 bits
+    using word_t = uint16_t; // Corresponding to byte_t
+
+    enum class ValueType : int {
+        Channel,
+        Value
+	};
+
+	enum class Limits : byte_t {
+		Min        = 0,
+		Max        = 127,
+		ChannelMax = 15
+	};
+
+	enum class Status : byte_t {
+		NoteOff          = 0x80, // 1000 0000
+		NoteOn           = 0x90, // 1001 0000
+		PolyKeyPressure  = 0xA0, // 1010 0000
+		ControllerChange = 0xB0, // 1011 0000
+		ProgramChange    = 0xC0, // 1100 0000
+		ChannelPressure  = 0xD0, // 1101 0000
+		PitchBend        = 0xE0  // 1110 0000
+	};
+
+	// http://www.indiana.edu/~emusic/etext/MIDI/chapter3_MIDI6.shtml
+	enum class Controller : byte_t {
+		BankSelect          = 0x00,
+		ModulationWheel     = 0x01,
+		Breath              = 0x02,
+		// Undefined        = 0x03,
+		Foot                = 0x04,
+		PortamentoTime      = 0x05,
+		DataEntryMsb        = 0x06,
+		MainVolume          = 0x07,
+		Balance             = 0x08,
+		// Undefined        = 0x09,
+		Pan                 = 0x0A,
+		Ch0                 = 0x0B,
+		Effect1             = 0x0C,
+		Effect2,
+		// Undefined        = 0x0E..0x0F
+		GeneralPurpose1     = 0x10,
+		GeneralPurpose2,
+		GeneralPurpose3,
+		GeneralPurpose4,
+		// Undefined        = 0x14..0x1F
+		// Lsb0..31         = 0x20..0x3F    // Rarely implemented
+		DamperPedalSustain  = 0x40,         // Data 1: 0..64 = Off, 64..127 = On
+		Portamento          = 0x41,
+		Sostenuto           = 0x42,
+		SoftPedal           = 0x43,
+		LegatoFootswitch    = 0x44,
+		Hold2               = 0x45,
+		SoundController1    = 0x46,         // Default: Sound Variation
+		SoundController2,                   // Default: Timbre/Harmonic Content
+		SoundController3,                   // Default: Release Time
+		SoundController4,                   // Default: Attack Time
+		SoundController5,                   // Default: Brightness
+		SoundController6,
+		SoundController7,
+		SoundController8,
+		SoundController9,
+		SoundController10,
+		GeneralPurpose5     = 0x50,
+		GeneralPurpose6,
+		GeneralPurpose7,
+		GeneralPurpose8,
+		PortamentoControl   = 0x54,
+		// Undefined        = 0x55..0x5A
+		EffectsDepth1       = 0x5B,         // Previously External Effects Depth
+		EffectsDepth2,                      // Previously Tremolo Depth
+		EffectsDepth3,                      // Previously Chorus Depth
+		EffectsDepth4,                      // Previously Detune Depth
+		EffectsDepth5,                      // Previously Phaser Depth
+		DataIncrement       = 0x60,
+		DataDecrement       = 0x61,
+		// NonRegistered... = 0x62..0x65
+		// Undefined        = 0x66..0x78
+
+		//  Channel Mode Messages
+		ResetAllControllers = 0x79,
+		LocalControl        = 0x7A,
+		AllNotesOff         = 0x7B,
+		OmniOff             = 0x7C,
+		OmniOn              = 0x7D,
+		MonoOnPolyOff       = 0x7E,
+		PolyOnMonoOff       = 0x7F
+	};
+
+    static byte_t Limit(const byte_t value, const ValueType type);
+
+    static void SplitStatusChannel(const byte_t status_channel,
+                                   Status& status,
+                                   byte_t& channel);
+    static byte_t CombineStatusChannel(const Status status, const byte_t channel);
+
+    static void SplitPitchBend(const word_t pitch_bend,
+                               byte_t& fine,
+                               byte_t& coarse);
+    static word_t CombinePitchBend(const byte_t fine, const byte_t coarse);
+};
+
+#endif // DJ_GLOVE_PC_MIDI_H_
+
