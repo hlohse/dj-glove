31b6e79db7cff9fc7e6b99d60c8d07a044b041c8
Henning Lohse <henning.lohse@gmail.com>
2014-08-06 11:37:31 +0200

    Defined sensor data protocol

--- PC/DjGloveSignal.h
+++ PC/DjGloveSignal.h
@@ -29,6 +29,9 @@ public:
     
     virtual int NumValues() const;
     virtual void FromNext(std::vector<char>& values);
+
+private:
+    bool punched_;
 };
 
 class EXPORT SensorsSignal : public DjGloveSignal {
@@ -42,6 +45,28 @@ public:
 private:
     // TODO: Adjust accordingly
     static const int num_midi_signals = 10;
+
+    bool button_push_0_;    // 1 Bit: pressed/not pressed
+    bool button_push_1_;
+    bool button_push_2_;
+    bool button_push_3_;
+    bool button_push_4_;
+    bool button_touch_0_;
+    bool button_touch_1_;
+    bool button_touch_2_;
+    bool button_touch_3_;
+    bool button_flip_;
+    int  poti_0_;           // 7 Bit: 0..127
+    int  poti_1_;
+    int  poti_2_;
+    int  distance_;
+    int  orientation_x_;
+    int  orientation_y_;
+    int  orientation_z_;
+    int  flex_;             // 6 Bit: 0..63
+    int  channel_;          // 3 Bit: 0..7 (LED)
+    int  program_;
+    // 71 Bit = 9 Byte total
 };
 
 #endif // DJ_GLOVE_PC_DJ_GLOVE_SIGNAL_H_
