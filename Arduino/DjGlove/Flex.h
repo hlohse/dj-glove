f2a54497b41e738f4c769df677bc107ed738e2c6
Henning Lohse <henning.lohse@gmail.com>
2014-09-15 12:57:18 +0200

    Implemented Flex with simple readout

--- Arduino/DjGlove/Flex.h
+++ Arduino/DjGlove/Flex.h
@@ -2,7 +2,21 @@
 #define DJ_GLOVE_ARDUINO_DJ_GLOVE_FLEX_H_
 
 class Flex {
-// TODO
+public:
+  Flex(const byte pin)
+  : m_pin(pin)
+  {
+    pinMode(m_pin, INPUT);
+  }
+  
+  int read() const
+  {
+    // Adjust 0..1023 to 0..127
+    return analogRead(m_pin) / 8;
+  }
+
+private:
+  byte m_pin;
 };
 
 #endif // DJ_GLOVE_ARDUINO_DJ_GLOVE_FLEX_H_
