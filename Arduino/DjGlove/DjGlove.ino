216633cc5835dc6f8cfceaa2f89cc8c99696dfd0
Henning Lohse <henning.lohse@gmail.com>
2014-09-15 13:29:21 +0200

    Test code to print data bytes via Serial

--- Arduino/DjGlove/DjGlove.ino
+++ Arduino/DjGlove/DjGlove.ino
@@ -10,11 +10,33 @@ Bluetooth bluetooth(Pins::bt_rx, Pins::bt_tx);
 
 void setup()  
 {
+  Serial.begin(115200);
+}
+
+void toBinaryString(const byte value, char* binary_string)
+{
+  for (int i = 0; i < 8; ++i) {
+    binary_string[i] = value >= (1 << (8 - (i + 1))) ? '1' : '0';
+  }
+  
+  binary_string[8] = '\0';
 }
 
 void loop()
 {
+  char binary_string[9];
+  
+  for (int i = 0; i < 11; ++i) {
+    toBinaryString(data.nextByte(), binary_string);
+    Serial.println(binary_string);
+  }
+  
+  Serial.println("=====");
+  delay(1000);
+  
+/*
   const byte next_data_byte = data.nextByte() + 1; // Avoid null byte
   bluetooth.write(next_data_byte);
+*/
 }
 
