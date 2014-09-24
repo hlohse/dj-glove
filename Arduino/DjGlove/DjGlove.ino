3dc71450d6293409bf9df2077ab593c1f9adcc9a
Henning Lohse <henning.lohse@gmail.com>
2014-09-24 11:12:47 +0200

    Reverted to test code

--- Arduino/DjGlove/DjGlove.ino
+++ Arduino/DjGlove/DjGlove.ino
@@ -16,42 +16,11 @@ void setup()
 
 void loop()
 {
-  Serial.println(glove.ultra_sound.read());
-  delay(500);
-  
-/*
-  for (unsigned char i = 0; i < 10; ++i) {
-    glove.led.setLeft(0);
-    glove.led.setRight(i);
-    glove.led.display();
-    delay(1000);
-  }
-*/
-  
-/*
-  glove.led.setLeft('P');
-  glove.led.setRight('E');
-  glove.led.display();
-  delay(1000);
-  
-  glove.led.setLeft('E');
-  glove.led.setRight('P');
-  glove.led.display();
-  delay(1000);
-*/
-  
-/*
   for (int i = 0; i < 11; ++i) {
     Serial.println(data.nextByte(), BIN);
   }
   
   Serial.println("=====");
   delay(300);
-*/
-  
-/*
-  const byte next_data_byte = data.nextByte() + 1; // Avoid null byte
-  bluetooth.write(next_data_byte);
-*/
 }
 
