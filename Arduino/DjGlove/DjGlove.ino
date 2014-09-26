9a30854d41bc24b817a5cd6255829b1cd022bab3
Henning Lohse <henning.lohse@gmail.com>
2014-09-26 13:27:11 +0200

    Added sync to DjGlove Arduino

--- Arduino/DjGlove/DjGlove.ino
+++ Arduino/DjGlove/DjGlove.ino
@@ -3,22 +3,33 @@
 #include "DjGlove.h"
 #include "Data.h"
 
+const char sync_signal = '!';
+
 DjGlove* glove = DjGlove::instance();
 Data data(glove);
+
+void sync()
+{
+  while (true) {
+    while (!Serial.available());
+    
+    if (Serial.read() == sync_signal) {
+      return;
+    }
+  }
+}
 
 void setup()  
 {
   glove->initialize();
   Serial.begin(115200);
+  sync();
 }
 
 void loop()
 {
   for (int i = 0; i < 11; ++i) {
-    //Serial.write(data.nextByte());
-    Serial.println(data.nextByte(), BIN);
+    Serial.write(data.nextByte());
   }
-  
-  delay(400);
 }
 
